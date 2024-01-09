10f081bb716ea83f2f8145081b205ef35ff49d07
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-08 19:19:49 -0600

    Se agregaron todas las variables del ejercicio 1. Así mismo se agregaron los console.log, añadiendo un cuantificador simple \n

--- JS/ejercicios.js
+++ JS/ejercicios.js
@@ -2,6 +2,45 @@
 
 // Variables de tipo number
 var edadDaniel = 28;
-console.log("La edad de Daniel es: " + edadDaniel);
+let cantidadItems = 5;
+var temperaturaAmbiente = 23;
+const velocidad = 55.5;
+let edadUsuario = 30;
+const gravedad = 9.8;
+var peso = 70.2;
+let distancia = 1500;
+const porcentajeDescuento = 10;
+var puntajeJugador = 1500;
 
-var 
+// Variables de tipo string
+let nombreProducto = "Zapatos";
+var marca = 'Nike';
+const ciudad = `Nueva York`;
+var direccion = "123 Calle Principal";
+let mensajeSaludo = "Hola, ¿cómo estás?";
+const tipoDocumento = 'Pasaporte';
+var nombreUsuario = "John Doe";
+let plataforma = 'Web';
+const nombreEmpresa = "Ejemplo Co.";
+
+// Variables de tipo boolean
+var esMayorEdad = true;
+let tieneDescuento = false;
+const usuarioRegistrado = true;
+var esDiaLaboral = false;
+let compraExitosa = true;
+const accesoPermitido = false;
+var isLoggedIn = true;
+let envioGratis = false;
+const cuentaActivada = true;
+var esAdmin = false;
+
+// Imprimir las variables
+console.log("Variables de tipo number:");
+console.log(edadDaniel, cantidadItems, temperaturaAmbiente, velocidad, edadUsuario, gravedad, peso, distancia, porcentajeDescuento, puntajeJugador);
+
+console.log("\nVariables de tipo string:");
+console.log(nombreProducto, marca, ciudad, direccion, mensajeSaludo, tipoDocumento, nombreUsuario, plataforma, nombreEmpresa);
+
+console.log("\nVariables de tipo boolean:");
+console.log(esMayorEdad, tieneDescuento, usuarioRegistrado, esDiaLaboral, compraExitosa, accesoPermitido, isLoggedIn, envioGratis, cuentaActivada, esAdmin);
